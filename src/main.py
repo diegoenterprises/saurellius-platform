f3be89776a7d4a5fb0425475f0446a6a2c3c2ca0
diegoenterprises <214093559+diegoenterprises@users.noreply.github.com>
2025-10-22 06:28:39 -0400

    Configure AWS RDS PostgreSQL and add psycopg2-binary to requirements

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ app.register_blueprint(user_bp, url_prefix='/api')
 app.register_blueprint(paystub_bp, url_prefix='/api')
 
 # uncomment if you need to use database
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://saurelliusadmin:ManusPassword123!@saurellius-db.cabe8skwsu5v.us-east-1.rds.amazonaws.com:5432/saurellius_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 with app.app_context():
